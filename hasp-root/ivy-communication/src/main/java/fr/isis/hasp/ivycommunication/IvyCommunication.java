d19a6cb8e5ba1d93a02a50bea3dcd1e7b6fda4da
vh.remy@gmail.com <vh.remy@gmail.com@bb7c21d6-5a92-ed7b-04bf-e8f9ec8db886>
2011-12-07 22:08:08 +0000

    Création

---
+++ hasp-root/ivy-communication/src/main/java/fr/isis/hasp/ivycommunication/IvyCommunication.java
@@ -0,0 +1,75 @@
+package fr.isis.hasp.ivycommunication;
+
+import fr.dgac.ivy.Ivy;
+import fr.dgac.ivy.IvyClient;
+import fr.dgac.ivy.IvyException;
+import fr.dgac.ivy.IvyMessageListener;
+
+public class IvyCommunication implements IvyCommunicationInterface {
+
+	private Ivy bus;
+
+	private IvyCommunication(String nomDuBus) {
+		if (nomDuBus != null && !"".equals(nomDuBus)) {
+			bus = new Ivy(nomDuBus, nomDuBus + "Ready", null);
+		} else {
+			bus = new Ivy("IvyBus", nomDuBus + "IvyBusReady", null);
+		}
+
+		String address = "172.18.3.255:2010";
+
+		try {
+			bus.start(address);
+		} catch (IvyException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Retourne un objet qui permet de communiquer avec le bus Ivy.
+	 * 
+	 * @param nomDuBus
+	 *            Nom de l'instance.
+	 * @return IvyCommunicationInterface.
+	 */
+	public static IvyCommunicationInterface getIvyCommunicationProxy(
+			String nomDuBus) {
+		return (IvyCommunicationInterface) new IvyCommunication(nomDuBus);
+	}
+
+	public void postMessage(String separateur, String[] message) {
+		try {
+
+			String messageString = "";
+			for (String contenu : message) {
+				if (contenu != null && !"".equals(contenu)) {
+					if (!"".equals(messageString)) {
+						messageString += separateur;
+					}
+					messageString += contenu;
+				}
+			}
+
+			if (!"".equals(messageString)) {
+				bus.sendMsg(messageString);
+			}
+
+		} catch (IvyException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void subscribeMessage(String regexp, IvyMessageListener listener) {
+		try {
+			bus.bindMsg(regexp, listener);
+		} catch (IvyException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void finalize() {
+		bus.stop();
+	}
+
+}
