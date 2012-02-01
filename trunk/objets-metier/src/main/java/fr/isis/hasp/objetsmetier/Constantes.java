c9e91d0786c85e193631cd145f3f436723ecf29f
vh.remy@gmail.com <vh.remy@gmail.com@bb7c21d6-5a92-ed7b-04bf-e8f9ec8db886>
2012-02-01 11:48:01 +0000

    Ajout de la constante temp et de la constante lumière

--- trunk/objets-metier/src/main/java/fr/isis/hasp/objetsmetier/Constantes.java
+++ trunk/objets-metier/src/main/java/fr/isis/hasp/objetsmetier/Constantes.java
@@ -7,7 +7,11 @@ public class Constantes {
 	public static final String ADRESSE = "172.18.3.255:2010";
 	
 	public static final String CAPTEUR_MOUVEMENT = "CapteurMouvement";
+	public static final String CAPTEUR_SON = "CapteurSon";
 	public static final String CHANGEMENT_PIECE= "ChangementPiece";
+	public static final String CAPTEUR_LUMIERE= "CapteurLumiere";
+	public static final String CAPTEUR_TEMPERATURE= "CapteurTemperature";
+	public static final String CAPTEUR_COUSSIN= "CapteurCoussin";
 
 	public static final String FORMAT_DATE= "yyyy.MM.dd.HH.mm.ss.SSSS";
 	
