18a6569fda4b82608ffbed24073519fc305b25e0
vh.remy@gmail.com <vh.remy@gmail.com@bb7c21d6-5a92-ed7b-04bf-e8f9ec8db886>
2012-02-01 17:11:56 +0000

    MAJ constantes

--- trunk/objets-metier/src/main/java/fr/isis/hasp/objetsmetier/Constantes.java
+++ trunk/objets-metier/src/main/java/fr/isis/hasp/objetsmetier/Constantes.java
@@ -1,20 +1,21 @@
 package fr.isis.hasp.objetsmetier;
 
 public class Constantes {
-	
+
 	public static final String NOM_PROJET = "HASP";
 	public static final String SEPARATEUR = "::";
 	public static final String ADRESSE = "172.18.3.255:2010";
-	
+
 	public static final String CAPTEUR_MOUVEMENT = "CapteurMouvement";
 	public static final String CAPTEUR_SON = "CapteurSon";
-	public static final String CHANGEMENT_PIECE= "ChangementPiece";
-	public static final String CAPTEUR_LUMIERE= "CapteurLumiere";
-	public static final String CAPTEUR_TEMPERATURE= "CapteurTemperature";
-	public static final String CAPTEUR_COUSSIN= "CapteurCoussin";
-	public static final String ENDORMISSEMENT= "Endormissement";
-	public static final String REVEIL= "Reveil";
+	public static final String CHANGEMENT_PIECE = "ChangementPiece";
+	public static final String CAPTEUR_LUMIERE = "CapteurLumiere";
+	public static final String CAPTEUR_TEMPERATURE = "CapteurTemperature";
+	public static final String CAPTEUR_COUSSIN = "CapteurCoussin";
+	public static final String ENDORMISSEMENT = "Endormissement";
+	public static final String REVEIL = "Reveil";
 
-	public static final String FORMAT_DATE= "yyyy.MM.dd.HH.mm.ss.SSSS";
-	
+	public static final String FORMAT_DATE = "yyyy.MM.dd.HH.mm.ss.SSSS";
+	public static final String FORMAT_DATE_IHM = "dd/MM/yyyy - HH:mm";
+
 }
