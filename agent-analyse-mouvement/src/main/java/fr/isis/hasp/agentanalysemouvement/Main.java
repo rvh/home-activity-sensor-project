f91a13e6daa6b46955bac981114b582c6d134b3b
Rémy Van Hoof <rvh@users.noreply.github.com>
2012-02-15 10:35:19 +0000

    MAJ capteur CO2

--- agent-analyse-mouvement/src/main/java/fr/isis/hasp/agentanalysemouvement/Main.java
+++ agent-analyse-mouvement/src/main/java/fr/isis/hasp/agentanalysemouvement/Main.java
@@ -26,92 +26,136 @@ public class Main extends Thread {
 		main.start();
 	}
 
+	private String nbPersonnes = "1";
+
 	@Override
 	public void run() {
 		final IvyCommunicationInterface ivy = IvyCommunication
-		.getIvyCommunicationProxy("AgentAnalyseMouvement");
+				.getIvyCommunicationProxy("AgentAnalyseMouvement");
 
 		Configuration config = new Configuration();
 		config.addEventTypeAutoName("fr.isis.hasp.objetsmetier");
-		final EPServiceProvider epService = EPServiceProviderManager
+
+		final EPServiceProvider epServiceMouvement = EPServiceProviderManager
 				.getDefaultProvider(config);
-		
+
+		final EPServiceProvider epServiceNBPersonnes = EPServiceProviderManager
+				.getDefaultProvider(config);
+
 		/**
-		 * On regarde une fenêtre de 4 évènements de type mouvement.
-		 * Si parmi les 4 évènements au moins 3 concernent la même pièce,
-		 * alors on stocke cet évènement dans le flux temporaire : FiltreMouvements.
+		 * On regarde une fenêtre de 4 évènements de type mouvement. Si parmi
+		 * les 4 évènements au moins 3 concernent la même pièce, alors on stocke
+		 * cet évènement dans le flux temporaire : FiltreMouvements.
 		 */
-		String expression1 = "insert into FiltreMouvements " +
-				"select numeroCapteur as id " +
-				"from Message.win:length(4) " +
-				"group by numeroCapteur " +
-				"having count(numeroCapteur) >= 3";
-		
-		EPStatement statement1 = epService.getEPAdministrator().createEPL(
-				expression1);
-		
+		String expression1 = "insert into FiltreMouvements "
+				+ "select numeroCapteur as id " + "from Message.win:length(4) "
+				+ "group by numeroCapteur "
+				+ "having count(numeroCapteur) >= 3";
+
+		EPStatement statement1 = epServiceMouvement.getEPAdministrator()
+				.createEPL(expression1);
+
 		/**
-		 * On regarde le chaque évènement du flux FiltreMouvements.
-		 * Si les deux derniers ne concernent pas la même pièce,
-		 * alors cet évènement est un changement de pièce.
+		 * On regarde le chaque évènement du flux FiltreMouvements. Si les deux
+		 * derniers ne concernent pas la même pièce, alors cet évènement est un
+		 * changement de pièce.
 		 */
-		String expression2 = "select b.id as id " +
-				"from pattern " +
-				"[every a=FiltreMouvements -> b=FiltreMouvements(a.id != b.id)]";
-		
-		EPStatement statement2 = epService.getEPAdministrator().createEPL(
-				expression2);
-		
+		String expression2 = "select b.id as id "
+				+ "from pattern "
+				+ "[every a=FiltreMouvements -> b=FiltreMouvements(a.id != b.id)]";
+
+		EPStatement statement2 = epServiceMouvement.getEPAdministrator()
+				.createEPL(expression2);
+
+		/**
+		 * 
+		 */
+		String expression3 = "select count(message), message "
+				+ "from Message.win:length(3) " + "group by message "
+				+ "having count(message) >= 2";
+
+		EPStatement statement3 = epServiceMouvement.getEPAdministrator()
+				.createEPL(expression3);
+
 		statement1.addListener(new StatementAwareUpdateListener() {
-		
+
 			public void update(EventBean[] newEvents, EventBean[] oldEvents,
 					EPStatement stmt, EPServiceProvider service) {
 				EventBean event = newEvents[0];
 				System.out.println("=> " + event.get("id"));
 			}
 		});
-		
+
 		statement2.addListener(new StatementAwareUpdateListener() {
-		
+
 			public void update(EventBean[] newEvents, EventBean[] oldEvents,
 					EPStatement stmt, EPServiceProvider service) {
 				EventBean event = newEvents[0];
-		
+
 				Message message = new Message();
 				message.setCategorieMessage(Constantes.CHANGEMENT_PIECE);
 				message.setDateMessage(new Date());
 				message.setNumeroCapteur((Integer) event.get("id"));
-		
-				ivy.postMessage(message);
+
+				if(nbPersonnes.equals("1")){
+					ivy.postMessage(message);
+				}
 			}
 		});
-		
+
+		statement3.addListener(new StatementAwareUpdateListener() {
+
+			public void update(EventBean[] newEvents, EventBean[] oldEvents,
+					EPStatement stmt, EPServiceProvider service) {
+				EventBean event = newEvents[0];
+				nbPersonnes = (String) event.get("message");
+//				System.out.println("nbPersonnes:" + nbPersonnes + " - "
+//						+ event.get("count(message)"));
+			}
+		});
+
 		ivy.subscribeMessage("^" + Constantes.NOM_PROJET
-				+ Constantes.SEPARATEUR + Constantes.CAPTEUR_MOUVEMENT
 				+ Constantes.SEPARATEUR + "(.*)", new IvyMessageListener() {
-		
+
 			public void receive(IvyClient arg0, String[] arg1) {
 				try {
-					String[] result = arg1[0].split(Constantes.SEPARATEUR);
-		
-					String[] numero = result[0].split("DETX");
-		
-					int num = Integer.parseInt(numero[0]);
-		
-					Message message = new Message();
-					message.setCategorieMessage(Constantes.CAPTEUR_MOUVEMENT);
-					message.setDateMessage(new Date());
-					message.setNumeroCapteur(num);
-		
-					epService.getEPRuntime().sendEvent(message);
-		
+					Message message = null;
+
+					try {
+						message = IvyCommunication.unSerialyzeMessage(arg1[0]);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+					if (message != null) {
+						if (message.getCategorieMessage().equals(
+								Constantes.CAPTEUR_MOUVEMENT)) {
+							epServiceMouvement.getEPRuntime()
+									.sendEvent(message);
+						} else if (message.getCategorieMessage().equals(
+								Constantes.CAPTEUR_CO2)) {
+							epServiceNBPersonnes.getEPRuntime().sendEvent(
+									message);
+						}
+
+					}
+					// String[] result = arg1[0].split(Constantes.SEPARATEUR);
+					//
+					// String[] numero = result[0].split("DETX");
+					//
+					// int num = Integer.parseInt(numero[0]);
+					//
+					// Message message = new Message();
+					// message.setCategorieMessage(Constantes.CAPTEUR_MOUVEMENT);
+					// message.setDateMessage(new Date());
+					// message.setNumeroCapteur(num);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		
+
 		});
-		
+
 		// TEST
 		//
 		// Message message = new Message();
@@ -132,7 +176,7 @@ public class Main extends Thread {
 		// epService.getEPRuntime().sendEvent(message2);
 		// epService.getEPRuntime().sendEvent(message);
 		// epService.getEPRuntime().sendEvent(message2);
-		 
+
 		//
 		// epService.getEPRuntime().sendEvent(message2);
 		// epService.getEPRuntime().sendEvent(message2);
@@ -144,5 +188,22 @@ public class Main extends Thread {
 		// epService.getEPRuntime().sendEvent(message);
 		// epService.getEPRuntime().sendEvent(message);
 		// epService.getEPRuntime().sendEvent(message);
+
+//		Message messageNB = new Message();
+//		messageNB.setCategorieMessage(Constantes.CAPTEUR_CO2);
+//		messageNB.setDateMessage(new Date());
+//		messageNB.setNumeroCapteur(0);
+//		messageNB.setMessage("1");
+//		Message messageNB2 = new Message();
+//		messageNB2.setCategorieMessage(Constantes.CAPTEUR_CO2);
+//		messageNB2.setDateMessage(new Date());
+//		messageNB2.setNumeroCapteur(0);
+//		messageNB2.setMessage("2");
+//
+//		epServiceNBPersonnes.getEPRuntime().sendEvent(messageNB);
+//		epServiceNBPersonnes.getEPRuntime().sendEvent(messageNB);
+//		epServiceNBPersonnes.getEPRuntime().sendEvent(messageNB);
+//		epServiceNBPersonnes.getEPRuntime().sendEvent(messageNB2);
+//		epServiceNBPersonnes.getEPRuntime().sendEvent(messageNB2);
 	}
 }
